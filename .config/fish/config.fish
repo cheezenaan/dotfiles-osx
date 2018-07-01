c8253e84ddcb49fa37c9faa3d81e2b164495726d
cheezenaan <cheezenaan@gmail.com>
2018-07-01 09:24:06 +0900

    chore(fish): Organize config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,25 +1,32 @@
-# Locale
+# General
+
+## Locale
 set -x LC_ALL ja_JP.UTF-8
 set -x LANG ja_JP.UTF-8
 
 set -x FISH_HOME $HOME/.config/fish
 
-# Homebrew
+## aliases
+source $FISH_HOME/alias.fish
+
+# Environment Variables
+
+## Homebrew
 set -x PATH /usr/local/bin $PATH
 
-# golang
+## golang
 set -x GOPATH $HOME
 set -x PATH $GOPATH/bin $PATH
 
-# nodebrew
+## nodebrew
 set -x PATH $HOME/.nodebrew/current/bin $PATH
 
-# Remove redundant paths
+## Remove redundant paths
 set -x PATH (echo $PATH | tr ' ' '\n' | sort -u)
 
-# fzf
+# Plugins
+
+
+## fzf
 set -x FZF_DEFAULT_OPTS '--height 80% --border --ansi --multi'
 set -x FZF_ALT_C_OPTS '--select-1 --exit-0'
-
-# Define aliases
-source $FISH_HOME/alias.fish
